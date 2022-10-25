395f01dcfc187ebf1825c79097f88f10e2d12cac
Karakhanyan Levon <levonkana@gmail.com>
2022-10-25 20:10:55 +0400

    added delete activity mehtod

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,33 +1,29 @@
-using AutoMapper;
-using Domain;
 using MediatR;
 using Persistence;
 
 namespace Application.Activities;
 
-public class Edit
+public class Delete
 {
     public class Command : IRequest
     {
-        public Activity Activity { get; set; }
+        public Guid Id { get; set; }
     }
 
-    public class Hander : IRequestHandler<Command>
+    public class Handler : IRequestHandler<Command>
     {
         private readonly DataContext _context;
-        private readonly IMapper _mapper;
 
-        public Hander(DataContext context, IMapper mapper)
+        public Handler(DataContext context)
         {
             _context = context;
-            _mapper = mapper;
         }
 
         public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
         {
-            var activity = await _context.Activities.FindAsync(request.Activity.Id);
+            var activity = await _context.Activities.FindAsync(request.Id);
 
-            _mapper.Map(request.Activity, activity);
+            _context.Remove(activity);
 
             await _context.SaveChangesAsync();
 
