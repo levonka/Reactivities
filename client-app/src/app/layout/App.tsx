76421b5cc5826f32d2888e5b4c9e7f25d9b2f443
Karakhanyan Levon <levonkana@gmail.com>
2022-10-25 22:19:26 +0400

    added useeffect in app.tsx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header, List } from 'semantic-ui-react';
+import { IActivity } from '../models/activity';
+import axios from 'axios';
 
 function App() {
+    const [activities, setActivities] = useState<IActivity[]>([]);
+
+    useEffect(() => {
+        axios.get<IActivity[]>('http://localhost:5000/api/activities').then(response => {
+            setActivities(response.data);
+        });
+    }, []);
+
     return (
         <div className="App">
             <Header as="h2" icon="users" content="Reactivities" />
-            <List className="App-header"></List>
+            <List className="App-header">
+                {activities.map(activity => (
+                    <List.Item key={activity.id}>{activity.title}</List.Item>
+                ))}
+            </List>
         </div>
     );
 }
