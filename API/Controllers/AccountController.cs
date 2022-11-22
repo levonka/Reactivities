909684f45132cc3dd2e5c6276a5fb75adb81ce4e
Karakhanyan Levon <levonkana@gmail.com>
2022-11-22 13:30:01 +0400

    added ActivityAttendees table

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -54,12 +54,14 @@ public class AccountController : ControllerBase
     {
         if (await _userManager.Users.AnyAsync(x => x.Email == registerDto.Email))
         {
-            return BadRequest("Email taken");
+            ModelState.AddModelError("email", "Email taken");
+            return ValidationProblem(ModelState);
         }
 
         if (await _userManager.Users.AnyAsync(x => x.UserName == registerDto.Username))
         {
-            return BadRequest("Username taken");
+            ModelState.AddModelError("username", "Username taken");
+            return ValidationProblem(ModelState);
         }
 
         var user = new AppUser
