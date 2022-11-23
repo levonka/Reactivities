a047eaca57ef33831687fe1e1dba205ebabf84cb
Karakhanyan Levon <levonkana@gmail.com>
2022-11-23 07:17:04 +0400

    added update attendance feature

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,5 +1,6 @@
 using Application.Core;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -28,13 +29,10 @@ public class List
         )
         {
             var activities = await _context.Activities
-                .Include(a => a.Attendees)
-                .ThenInclude(u => u.AppUser)
+                .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
                 .ToListAsync(cancellationToken);
 
-            var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
-
-            return Result<List<ActivityDto>>.Success(activitiesToReturn);
+            return Result<List<ActivityDto>>.Success(activities);
         }
     }
 }
