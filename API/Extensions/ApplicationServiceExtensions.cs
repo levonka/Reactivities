ea4770aafcea89b09b0a4e246ba19d77d60eb192
Karakhanyan Levon <levonkana@gmail.com>
2022-11-23 18:08:25 +0400

    added photoAccessor

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -35,6 +35,7 @@ public static class ApplicationServicesExtensions
         services.AddMediatR(typeof(List.Handler).Assembly);
         services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         services.AddScoped<IUserAccessor, UserAccessor>();
+        services.AddScoped<IPhotoAccessor, PhotoAccessor>();
         services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
         return services;
